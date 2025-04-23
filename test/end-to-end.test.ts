fe88d6f1a34adcbe415f639b0723a352d2ee1f49
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-23 19:47:17 +0300

    Added expected output to e2e test

--- test/end-to-end.test.ts
+++ test/end-to-end.test.ts
@@ -1,14 +1,21 @@
-import path from 'path';
-import {it, describe, expect} from 'vitest';
-import {execa } from 'execa';
+import path from "path";
+import { it, describe, expect } from "vitest";
+import { execa } from "execa";
 
-describe('End-to-end test', () => {
+describe("End-to-end test", () => {
+  it("should read and parse given file", async () => {
+    const indexPath = path.resolve(__dirname, "..", "src", "index.ts");
+    const filePath = path.resolve(__dirname, "..", "patterns", "block.rle");
 
-    it ('should read and parse given file', async () => {
-        const indexPath = path.resolve(__dirname, '..', 'src', 'index.ts');
-        
-        const { stdout } = await execa('ts-node', [indexPath, 'test-path', '123']);
+    const { stdout } = await execa("ts-node", [indexPath, filePath, "123"]);
 
-        console.log('stdout', stdout);
-    });
+    const expected = `
+#N Block
+#C An extremely common 4-cell still life.
+#C www.conwaylife.com/wiki/index.php?title=Block
+x = 2, y = 2, rule = B3/S23
+2o$2o!
+`;
+    expect(stdout).toContain(expected);
+  });
 });
