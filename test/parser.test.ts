b487850d126701db14235a55162e81e9ce6a0059
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-24 14:28:02 +0300

    Test for 2x1 pattern

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -40,7 +40,15 @@ describe("Parser", () => {
     expect(parsedPattern).toEqual(expected);
   });
 
-  it("should parse 2x2 pattern ", () => {
-    const patternLine = "2o$2o!";
+  it("should parse 2x1 pattern", () => {
+    const patternLine = "2o!";
+    const expected = {
+        0: {
+            0: Cell.ALIVE,
+            1: Cell.ALIVE,
+        }
+    }
+    const parsedPattern = parsePatternLine(patternLine);
+    expect(parsedPattern).toEqual(expected);
   });
 });
