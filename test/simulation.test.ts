1898087efeb0586aaf52fa9430b54d0303e071fd
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-24 19:31:41 +0300

    Add test for reviving a dead cell with three live neighbours in Game of Life simulation

--- test/simulation.test.ts
+++ test/simulation.test.ts
@@ -93,4 +93,22 @@ describe("Game of Life Simulation", () => {
     const result = new Simulation(grid, 3, 3).nextGeneration();
     expect(result).toEqual(expectedSimulation);
   });
+
+  it("should revive a dead cell with exactly three live neighbours (3x3)", () => {
+    const grid = {
+        0: { 0: Cell.DEAD, 1: Cell.ALIVE, 2: Cell.ALIVE },
+        1: { 0: Cell.DEAD, 1: Cell.ALIVE, 2: Cell.ALIVE },
+        2: { 0: Cell.DEAD, 1: Cell.ALIVE, 2: Cell.DEAD },
+      };
+  
+      const expectedGridState = {
+        0: { 0: Cell.DEAD, 1: Cell.ALIVE, 2: Cell.ALIVE },
+        1: { 0: Cell.ALIVE, 1: Cell.DEAD, 2: Cell.DEAD },
+        2: { 0: Cell.DEAD, 1: Cell.ALIVE, 2: Cell.ALIVE },
+      };
+      const expectedSimulation = new Simulation(expectedGridState, 3, 3);
+  
+      const result = new Simulation(grid, 3, 3).nextGeneration();
+      expect(result).toEqual(expectedSimulation);
+  });
 });
