0222eddb3e2727967c9e1995c40e30081dc70325
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-24 18:30:38 +0300

    Refactor loadRLEFile to parse pattern lines after header and validate line length

--- src/parser.ts
+++ src/parser.ts
@@ -79,16 +79,18 @@ export const loadRLEFile = (filePath: string): LoadRLEResult => {
 
     const headerLine = lines[linesBeforeHeader.length];
     const header = parseHeader(headerLine || "");
-    const patternLines = lines.slice(linesBeforeHeader.length + 1);
+    const linesAfterHeader = lines.slice(linesBeforeHeader.length + 1);
 
-    if (patternLines.filter((line) => line.length > 70).length > 0) {
+    if (linesAfterHeader.filter((line) => line.length > 70).length > 0) {
       throw new Error("Line exceeds 70 characters");
     }
+
+    const pattern = parsePattern(linesAfterHeader.join(""), header.width, header.height);
 
     return {
       linesBeforeHeader,
       ...header,
-      pattern: {},
+      pattern,
       linesAfterPattern: [],
     };
   } catch (error) {
