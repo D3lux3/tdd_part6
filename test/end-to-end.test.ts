ff487faa259dfb289684e7e51ca0bfcf35d3bd12
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-28 17:19:38 +0300

    Update end-to-end test to simulate shape from file and adjust expected output

--- test/end-to-end.test.ts
+++ test/end-to-end.test.ts
@@ -4,18 +4,18 @@ import { execa } from "execa";
 import '../src/index.ts?raw';
 
 describe("End-to-end test", () => {
-  it("should read and parse given file", async () => {
+  it("should read, parse and simulate shape from given file", async () => {
     const indexPath = path.resolve(__dirname, "..", "src", "index.ts");
     const filePath = path.resolve(__dirname, "..", "patterns", "blinker.rle");
 
-    const { stdout } = await execa("ts-node", [indexPath, filePath, "1"]);
+    const { stdout } = await execa("ts-node", [indexPath, filePath, "2"]);
 
     const expected = `#N Blinker
 #O John Conway
 #C A period 2 oscillator that is the smallest and most common oscillator.
 #C www.conwaylife.com/wiki/index.php?title=Blinker
-x = 1, y = 3
-o$o$o!`
+x = 3, y = 1
+ooo!`
     expect(stdout).toEqual(expected);
     expect(stdout).toContain("N Blinker");
   });
