fa36fe5f144b6ab96fc2fff9c057824bce0bc1ed
Daniyal Haapaniemi <daniyal.haapaniemi@futurice.com>
2025-04-28 17:39:17 +0300

    Test for text splitting in the output

--- test/simulation.test.ts
+++ test/simulation.test.ts
@@ -232,4 +232,91 @@ describe("Game of Life Simulation", () => {
     const result = sim.toString();
     expect(result).toEqual(expectedShapeString);
   });
+
+  it("should split the pattern two multiple lines if pattern exceeds 70 character", () => {
+    const bigPattern = {
+      0: {
+        0: Cell.DEAD,
+        1: Cell.ALIVE,
+        2: Cell.DEAD,
+        3: Cell.ALIVE,
+        4: Cell.DEAD,
+        5: Cell.ALIVE,
+        6: Cell.DEAD,
+        7: Cell.ALIVE,
+        8: Cell.DEAD,
+        9: Cell.ALIVE,
+        10: Cell.DEAD,
+        11: Cell.ALIVE,
+        12: Cell.DEAD,
+        13: Cell.ALIVE,
+        14: Cell.DEAD,
+        15: Cell.ALIVE,
+        16: Cell.DEAD,
+        17: Cell.ALIVE,
+        18: Cell.DEAD,
+        19: Cell.ALIVE,
+        20: Cell.DEAD,
+        21: Cell.ALIVE,
+        22: Cell.DEAD,
+        23: Cell.ALIVE,
+        24: Cell.DEAD,
+        25: Cell.ALIVE,
+        26: Cell.DEAD,
+        27: Cell.ALIVE,
+        28: Cell.DEAD,
+        29: Cell.ALIVE,
+        30: Cell.DEAD,
+        31: Cell.ALIVE,
+        32: Cell.DEAD,
+        33: Cell.ALIVE,
+        34: Cell.DEAD,
+        35: Cell.ALIVE,
+        36: Cell.DEAD,
+        37: Cell.ALIVE,
+        38: Cell.DEAD,
+        39: Cell.ALIVE,
+        40: Cell.DEAD,
+        41: Cell.ALIVE,
+        42: Cell.DEAD,
+        43: Cell.ALIVE,
+        44: Cell.DEAD,
+        45: Cell.ALIVE,
+        46: Cell.DEAD,
+        47: Cell.ALIVE,
+        48: Cell.DEAD,
+        49: Cell.ALIVE,
+        50: Cell.DEAD,
+        51: Cell.ALIVE,
+        52: Cell.DEAD,
+        53: Cell.ALIVE,
+        54: Cell.DEAD,
+        55: Cell.ALIVE,
+        56: Cell.DEAD,
+        57: Cell.ALIVE,
+        58: Cell.DEAD,
+        59: Cell.ALIVE,
+        60: Cell.DEAD,
+        61: Cell.ALIVE,
+        62: Cell.DEAD,
+        63: Cell.ALIVE,
+        64: Cell.DEAD,
+        65: Cell.ALIVE,
+        66: Cell.DEAD,
+        67: Cell.ALIVE,
+        68: Cell.DEAD,
+        69: Cell.ALIVE,
+        70: Cell.DEAD,
+        71: Cell.ALIVE,
+        72: Cell.DEAD,
+        73: Cell.ALIVE,
+        74: Cell.DEAD
+      }
+    }
+    
+    const sim = new Simulation(bigPattern);
+    const expectedShapeString = `obobobobobobobobobobobobobobobobobobobobobobobobobobobobobobobobobobo$bobo!`;
+    const result = sim.toString();
+    expect(result).toEqual(expectedShapeString);
+  });
 });
